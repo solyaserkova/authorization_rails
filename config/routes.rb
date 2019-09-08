eaf91da8d46376379394eaac88f30c59fc1b4f77
Olya <solyaserkova@gmail.com>
2019-09-09 01:09:40 +0300

    views and controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root 'welcome#index'
+
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout',registration: 'registrations', sign_up: 'cmon_let_me_in' }
+
+  # devise_scope :user do
+  #   match "/auth/register", to: "devise/registrations#create", via: :post
+  #   match "/auth/register/cmon_let_me_in", to: "devise/registrations#new", via: :get
+  # end
 end
