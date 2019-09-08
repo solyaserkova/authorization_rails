d5c766eeb5fa42e0b990e77235b3b87d335b26e3
Olya <solyaserkova@gmail.com>
2019-09-09 01:11:47 +0300

    welcome controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
+  get 'welcome/index'
   root 'welcome#index'
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout',registration: 'registrations', sign_up: 'cmon_let_me_in' }
